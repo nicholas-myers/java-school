92577fd31bdcacc1b864e82d22f6a5cef2757fed
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-06-15 14:27:05 -0700

    set up all file structure and changed imports and set up error message changes with the return message to be the new methods instead of defaults

---
+++ schools/src/main/java/com/lambdaschool/schools/handlers/RestExceptionHandler.java
@@ -0,0 +1,71 @@
+package com.lambdaschool.schools.handlers;
+
+import com.lambdaschool.schools.exceptions.ResourceFoundException;
+import com.lambdaschool.schools.exceptions.ResourceNotFoundException;
+import com.lambdaschool.schools.models.ErrorDetail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler
+{
+   @Autowired
+   private HelpFunctions helper;
+
+
+   public RestExceptionHandler()
+   {
+      super();
+   }
+
+   @ExceptionHandler(ResourceNotFoundException.class)
+   public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe)
+   {
+      ErrorDetail errorDetail = new ErrorDetail();
+      errorDetail.setTimestamp( new Date());
+      errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+      errorDetail.setDetail(rnfe.getMessage());
+      errorDetail.setTitle("Resource Not Found");
+      errorDetail.setDeveloperMessage(rnfe.getClass().getName());
+      errorDetail.setErrors(helper.getConstraintViolation(rnfe));
+
+      return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+   }
+
+   @Override
+   protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request)
+   {
+      ErrorDetail errorDetail = new ErrorDetail();
+      errorDetail.setTimestamp(new Date());
+      errorDetail.setStatus(status.value());
+      errorDetail.setTitle("Rest Internal Exception");
+      errorDetail.setDetail("Found an issue with School: " + ex.getMessage());
+      errorDetail.setDeveloperMessage(ex.getClass().getName());
+      errorDetail.setErrors(helper.getConstraintViolation(ex));
+      return new ResponseEntity<>(errorDetail, null, status);
+   }
+
+   @ExceptionHandler(ResourceFoundException.class)
+   public ResponseEntity<?> handleResourceFoundException(ResourceFoundException rfe)
+   {
+      ErrorDetail errorDetail = new ErrorDetail();
+      errorDetail.setTimestamp( new Date());
+      errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
+      errorDetail.setTitle("Resource Found Exception");
+      errorDetail.setDeveloperMessage(rfe.getClass().getName());
+      errorDetail.setErrors(helper.getConstraintViolation(rfe));
+
+      return new ResponseEntity<>(errorDetail, null, HttpStatus.BAD_REQUEST);
+   }
+}
